ad59114adffc2c96a7002387d94ab07c670efc80
Neota Moe <neota.moe@gmail.com>
2019-01-04 18:32:05 -0600

    adjusting content of popup

--- food-quiz-first/background.js
+++ food-quiz-first/background.js
@@ -1,16 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
     // add an action here
     chrome.tabs.onCreated.addListener(function (tab){
-        console.log('new tab created!');
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://floating-cove-50492.herokuapp.com/api/questions", false);
-        xhr.send();
+        // console.log('new tab created!');
+        // var xhr = new XMLHttpRequest();
+        // xhr.open("GET", "https://floating-cove-50492.herokuapp.com/api/questions", false);
+        // xhr.send();
 
-        var result = JSON.parse(xhr.responseText);
-        result = result.data[0];
-        console.log(result);
-        console.log(result.question)
+        // var result = JSON.parse(xhr.responseText);
+        // result = result.data[0];
+        // console.log(result);
+        // console.log(result.question)
     });
     
-    chrome.runtime.sendMessage({}, function(response) { document.body.style.backgroundcolor="red"; });
+    // chrome.runtime.sendMessage({}, function(response) { document.body.style.backgroundcolor="red"; });
 });
--- food-quiz-first/popup.js
+++ food-quiz-first/popup.js
@@ -1,1 +1,9 @@
 console.log('this is from popup.js');
+
+$(document).ready(function(){
+    $('body').on('click', 'a', function(){
+      chrome.tabs.create({url: $(this).attr('href')});
+      return false;
+    });
+    
+});
